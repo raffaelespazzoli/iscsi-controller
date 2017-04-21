07c7a32cc5426dc86c7253767941f35636d3dd0a
raffaelespazzoli <raffaele.spazzoli@gmail.com>
2017-04-21 14:51:28 -0400

    update

--- provisioner/iscsi-provisioner.go
+++ provisioner/iscsi-provisioner.go
@@ -3,33 +3,122 @@ package provisioner
 import (
 	"github.com/kubernetes-incubator/external-storage/lib/controller"
 	"k8s.io/client-go/pkg/api/v1"
+	"net/rpc"
+	"net/rpc/jsonrpc"
 )
 
-type iscsiProvisioner struct {
-	// The directory to create PV-backing directories in
-	pvDir string
+var log = logrus.New()
 
-	// Identity of this hostPathProvisioner, set to node's name. Used to identify
-	// "this" provisioner's PVs.
-	identity string
+type vol_createArgs struct {
+  pool string
+  name string
+  size int
 }
 
-func NewiscsiProvisioner() controller.Provisioner {
+type vol_destroyArgs struct {
+  pool string
+  name string
+}
+
+type export_createArgs struct {
+  pool string,
+  vol string
+  initiator_wwn string 
+  lun string
+}
+
+type export_destroyArgs struct {
+  pool string
+  vol string
+  initiator_wwn string
+}
+
+type iscsiProvisioner struct {
+	client rpc.Client
+	pool string
+	initiator_wwn string
+}
+
+func NewiscsiProvisioner(url string, pool string) controller.Provisioner {
+
+  initLog()
+	client, err := jsonrpc.Dial(network, url)
+
+	if err != nil {
+		log.Fatalln(err)
+	}
+	log.Debugln("targetd client created")
 
 	return &iscsiProvisioner{
-		pvDir:    "/tmp/hostpath-provisioner",
-		identity: "ciao",
+		pvDir: client,
+		pool:  pool,
 	}
 }
 
 // Provision creates a storage asset and returns a PV object representing it.
 func (p *iscsiProvisioner) Provision(options controller.VolumeOptions) (*v1.PersistentVolume, error) {
+	lun, vol := p.createVolume(options)
+	if err != nil {
+		return nil, err
+	}
 
-	return nil, nil
+	annotations := make(map[string]string)
+	annotations["volume_name"] = vol
+//	annotations[annExportBlock] = exportBlock
+//	annotations[annExportID] = strconv.FormatUint(uint64(exportID), 10)
+//	annotations[annProjectBlock] = projectBlock
+//	annotations[annProjectID] = strconv.FormatUint(uint64(projectID), 10)
+//	if supGroup != 0 {
+//		annotations[VolumeGidAnnotationKey] = strconv.FormatUint(supGroup, 10)
+//	}
+//	annotations[annProvisionerID] = string(p.identity)
+
+	pv := &v1.PersistentVolume{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:        options.PVName,
+			Labels:      map[string]string{},
+			Annotations: annotations,
+		},
+		Spec: v1.PersistentVolumeSpec{
+			PersistentVolumeReclaimPolicy: options.PersistentVolumeReclaimPolicy,
+			AccessModes:                   options.PVC.Spec.AccessModes,
+			Capacity: v1.ResourceList{
+				v1.ResourceName(v1.ResourceStorage): options.PVC.Spec.Resources.Requests[v1.ResourceName(v1.ResourceStorage)],
+			},
+			PersistentVolumeSource: v1.PersistentVolumeSource{
+			  ISCSI: &v1.ISCSIVolumeSource {
+			    TargetPortal: options.Parameters["targetPortal"],
+			    IQN: options.Parameters["iqn"],
+			    ISCSIInterface: options.Parameters["iscsiInterface"], 
+			    Lun: options.PVName,
+			    ReadOnly: false,
+			    FSType: "zfs",
+			  },
+			},
+		},
+	}
+	return pv, nil
 }
 
 // Delete removes the storage asset that was created by Provision represented
 // by the given PV.
 func (p *iscsiProvisioner) Delete(volume *v1.PersistentVolume) error {
-	return nil
+  //vol from the annotation
+  export_destroy(p.pool, vol, p.initiator_wwn)
+  vol_destroy(p.pool, options.PVName)
+
+	return pv, nil
 }
+
+func initLog() {
+	var err error
+	log.Level, err = logrus.ParseLevel(viper.GetString("log-level"))
+	if err != nil {
+		log.Fatalln(err)
+	}
+}
+
+func (p *iscsiProvisioner) createVolume(options controller.VolumeOptions) (lun string, vol string) {  
+  vol:=vol_create(p.pool, options.PVName, size)
+  export_create(p.pool, vol, p.initiator_wwn, options.PVName)
+} 
